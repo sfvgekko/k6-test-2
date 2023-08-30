189eefeb50d3b53b101c918f117e2399b4a69598
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-08-30 14:19:12 +0200

    Update testApp1.js

--- testfiles/testApp1.js
+++ testfiles/testApp1.js
@@ -1,7 +1,17 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check } from 'k6';
+
+export const options = {
+  stages: [
+    { target: 200, duration: '30s' },
+    { target: 0, duration: '30s' },
+  ],
+};
 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+  const result = http.get('https://test-api.k6.io/public/crocodiles/');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
 }
+
