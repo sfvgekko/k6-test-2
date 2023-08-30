754e21f0b2267d66d30fc49e2a2da1357fb3e64c
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-08-30 14:19:23 +0200

    Update testApp2.js

--- testfiles/testApp2.js
+++ testfiles/testApp2.js
@@ -1,13 +1,18 @@
 import http from 'k6/http';
-
+import { check } from 'k6';
 
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-  },
+  stages: [
+    { target: 200, duration: '30s' },
+    { target: 0, duration: '30s' },
+  ],
 };
 
 export default function () {
-  http.get('https://test-api.k6.io/public/crocodiles/1/');
+  const result = http.get('https://test-api.k6.io/public/crocodiles/');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
 }
+
+
