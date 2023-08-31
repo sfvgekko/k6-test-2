1f31ac06e7b0a895067c95a0f06e690dde497d77
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-08-31 11:12:56 +0200

    Update testApp1.js

--- testfiles/testApp1.js
+++ testfiles/testApp1.js
@@ -6,6 +6,13 @@ export const options = {
     { target: 200, duration: '35s' },
     { target: 0, duration: '30s' },
   ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  }, 
+  tags: {
+    name: 'testApp1',
+  },  
 };
 
 export default function () {
