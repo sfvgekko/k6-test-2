efe03dc65f03b35ac7e88a5651722571e4fcb2eb
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-08-31 14:31:09 +0200

    Update apptraining-micro1-loadtest.js

--- testfiles/apptraining-micro1-loadtest.js
+++ testfiles/apptraining-micro1-loadtest.js
@@ -3,9 +3,9 @@ import { check } from 'k6';
 
 export const options = {
   stages: [
-    { target: 100, duration: '20s' },
-    { target: 200, duration: '20s' },
-    { target: 300, duration: '20s' },
+    { target: 10, duration: '20s' },
+    { target: 20, duration: '20s' },
+    { target: 30, duration: '20s' },
   ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
