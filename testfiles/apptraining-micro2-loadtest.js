4ded80f25e5617cb5a72ab948740fe4785b8b636
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-09-01 09:58:45 +0200

    RUN

--- testfiles/apptraining-micro2-loadtest.js
+++ testfiles/apptraining-micro2-loadtest.js
@@ -3,14 +3,13 @@ import { check } from 'k6';
 
 export const options = {
   stages: [
-    { target: 100, duration: '35s' },
+    { target: 100, duration: '30s' },
   ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
   }, 
 };
-
 
 export default function () {
   const result = http.get('https://test-api.k6.io/public/crocodiles/', {
