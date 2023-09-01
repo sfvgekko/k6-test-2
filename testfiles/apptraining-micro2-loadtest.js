e9fc6b6098e52a5c295fc9105050b8b8884c25f3
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-09-01 11:26:53 +0200

    Update apptraining-micro2-loadtest.js

--- testfiles/apptraining-micro2-loadtest.js
+++ testfiles/apptraining-micro2-loadtest.js
@@ -3,7 +3,7 @@ import { check } from 'k6';
 
 export const options = {
   stages: [
-    { target: 100, duration: '30s' },
+    { target: 100, duration: '60s' },
   ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
@@ -12,11 +12,14 @@ export const options = {
 };
 
 export default function () {
-  const result = http.get('https://test-api.k6.io/public/crocodiles/', {
+  const employeeNumber = Math.floor(Math.random() * 5) + 1;
+
+  const result = http.get(`http://localhost:8085/api/employee-report/employee/${employeeNumber}/month/6/year/2023`, {
     tags: {
       test: 'apptraining-micro2-loadtest'
     }
   });
+
   check(result, {
     'http response status code is 200': result.status === 200,
   });
