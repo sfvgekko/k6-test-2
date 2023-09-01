6f62333d2864c36837146c66a2f45ebdbb563d93
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-09-01 10:00:29 +0200

    RUN

--- testfiles/apptraining-micro1-loadtest.js
+++ testfiles/apptraining-micro1-loadtest.js
@@ -11,15 +11,15 @@ export const options = {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
   }, 
-  tags: {
-    test: 'apptraining-micro1-loadtest',
-  },  
 };
 
 
 export default function () {
-  const result = http.get('https://test-api.k6.io/public/crocodiles/');
-  check(result, {
+  const result = http.get('https://test-api.k6.io/public/crocodiles/', {
+    tags: {
+      test: 'apptraining-micro1-loadtest'
+    }
+  });  check(result, {
     'http response status code is 200': result.status === 200,
   });
 }
