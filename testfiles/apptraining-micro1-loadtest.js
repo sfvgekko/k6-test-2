9c106be620493016f15e2726a5a81d06309188eb
Santiago Ferreira <35508408+sfvgekko@users.noreply.github.com>
2023-09-01 10:39:22 +0200

    RUN

--- testfiles/apptraining-micro1-loadtest.js
+++ testfiles/apptraining-micro1-loadtest.js
@@ -10,16 +10,19 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-  }, 
+  },
 };
 
-
 export default function () {
-  const result = http.get('https://test-api.k6.io/public/crocodiles/', {
+  const employeeNumber = Math.floor(Math.random() * 5) + 1;
+
+  const result = http.get(`http://localhost:8080/api/employee-report/employee/${employeeNumber}/month/7/year/2023`, {
     tags: {
       test: 'apptraining-micro1-loadtest'
     }
-  });  check(result, {
+  });
+
+  check(result, {
     'http response status code is 200': result.status === 200,
   });
 }
